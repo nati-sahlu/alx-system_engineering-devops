f59939186b2dc4daf033e0361e35a1c9442e4f79
nati-sahlu <natansahlu5@gmai.com>
2025-02-11 01:27:26 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,43 +1,26 @@
 #!/usr/bin/python3
-""" Script for parsing web data from an API
-"""
-import json
+"""Gather data from an API"""
+
 import requests
 import sys
 
+
 if __name__ == "__main__":
-    base_url = 'https://jsonplaceholder.typicode.com/'
+    # Define the URL for the REST API
+    url = "https://jsonplaceholder.typicode.com/"
 
-    # Validate input
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: {} employee_id".format(sys.argv[0]))
-        exit(1)
+    # send a GET request to retrieve user info
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
 
-    employee_id = sys.argv[1]
+    # send a GET request to retrive the TODO list
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    # Fetch user details
-    user_url = "{}users/{}".format(base_url, employee_id)
-    response = requests.get(user_url)
-    if response.status_code != 200:
-        print("Error: Invalid Employee ID")
-        exit(1)
+    # filter completed TODO list and store titles in a list
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
 
-    user = response.json()
-    if not user:
-        print("Error: No user found")
-        exit(1)
+    # print employee's name, completed tasks & total no of tasks
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
 
-    name = user.get('name', 'Unknown')
-
-    # Fetch user tasks
-    tasks_url = "{}todos?userId={}".format(base_url, employee_id)
-    response = requests.get(tasks_url)
-    todos = response.json()
-
-    completed_tasks = [task for task in todos if task.get('completed')]
-
-    # Print output
-    print("Employee {} is done with tasks({}/{}):".format(name, len(completed_tasks), len(todos)))
-    for task in completed_tasks:
-        print("\t {}".format(task.get('title')))
-
+    # print the titles of completed tasks with indentation
+    [print("\t {}".format(c)) for c in completed]
