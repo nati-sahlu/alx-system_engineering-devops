b3a71939f16a0d8fe7cded52f19dd20b0c9fda56
nati-sahlu <natansahlu5@gmai.com>
2025-02-11 01:24:30 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,43 @@
 #!/usr/bin/python3
-"""script for parsing web data from an api
+""" Script for parsing web data from an API
 """
+import json
+import requests
+import sys
+
 if __name__ == "__main__":
-    import json
-    import requests
-    import sys
     base_url = 'https://jsonplaceholder.typicode.com/'
-    try:
-        employee_id = sys.argv[1]
-    except:
-        print('Usage: {} employee_id'.format(sys.argv[0]))
+
+    # Validate input
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: {} employee_id".format(sys.argv[0]))
         exit(1)
 
-    # grab the info about the user
-    url = base_url + 'users?id={}'.format(employee_id)
-    response = requests.get(url)
-    user = json.loads(response.text)
-    name = user[0].get('name')
+    employee_id = sys.argv[1]
 
-    # grab the info about the user's tasks
-    url = base_url + 'todos?userId={}'.format(employee_id)
-    response = requests.get(url)
-    objs = json.loads(response.text)
-    completed = 0
-    completed_tasks = []
-    for obj in objs:
-        if obj.get('completed'):
-            completed_tasks.append(obj)
-            completed += 1
+    # Fetch user details
+    user_url = "{}users/{}".format(base_url, employee_id)
+    response = requests.get(user_url)
+    if response.status_code != 200:
+        print("Error: Invalid Employee ID")
+        exit(1)
 
-    # print the output about user's task completion
-    print("{} is done with tasks({}/{}):".format(name, completed, len(objs)))
-    # print the output title of completed tasks
+    user = response.json()
+    if not user:
+        print("Error: No user found")
+        exit(1)
+
+    name = user.get('name', 'Unknown')
+
+    # Fetch user tasks
+    tasks_url = "{}todos?userId={}".format(base_url, employee_id)
+    response = requests.get(tasks_url)
+    todos = response.json()
+
+    completed_tasks = [task for task in todos if task.get('completed')]
+
+    # Print output
+    print("Employee {} is done with tasks({}/{}):".format(name, len(completed_tasks), len(todos)))
     for task in completed_tasks:
         print("\t {}".format(task.get('title')))
+
